2bfe1c4d9280731aabf87ca81b18da692def2ec9
besson <flpbesson@gmail.com>
2012-03-31 14:44:01 -0300

    training

---
+++ training/rehearsal-demo/src/test/java/eu/choreos/WSMockDemo.java
@@ -0,0 +1,38 @@
+package eu.choreos;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import eu.choreos.vv.clientgenerator.Item;
+import eu.choreos.vv.clientgenerator.WSClient;
+import eu.choreos.vv.servicesimulator.MockResponse;
+import eu.choreos.vv.servicesimulator.WSMock;
+
+public class WSMockDemo {
+	
+	final String greetingWSDL = "http://localhost:9080/greeting?wsdl";	
+	
+	@Test
+	public void shouldChangeTheNameIfItIsJohn() throws Exception {
+	
+		WSMock mock = new WSMock("greetingAWSMock", "http://localhost:1234/greetingAWS?wsdl","5000", true);
+		// Mock is also used as interceptor
+		
+		MockResponse response = new MockResponse().whenReceive("john").replyWith("hey Mary,");
+		mock.start();
+		
+		
+		MockResponse response1 = new MockResponse().whenReceive("*").replyWith("hey world,");
+		mock.returnFor("sayHi", response, response1);
+		
+		
+		WSClient client = new WSClient(greetingWSDL);
+		Item clientResponse = client.request("hi", "john");
+		
+		Item clientResponse1 = client.request("hi", "brian");
+		
+		assertEquals("hey Mary, how are you doing bro?", clientResponse.getChild("return").getContent());
+		assertEquals("hey world, how are you doing bro?", clientResponse1.getChild("return").getContent());
+	}
+}
