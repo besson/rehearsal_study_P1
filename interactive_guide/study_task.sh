b1e8d6294feec5e38c0c797fd615d875ccc321c2
Felipe Besson <flpbesson@gmail.com>
2012-04-04 14:20:14 -0300

    renaming study_step.sh to study_task.sh

---
+++ interactive_guide/study_task.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ROLE="interactiveGuide"
+
+LIB="$HOME/.m2/repository/com/orientechnologies/orient-commons/1.0rc5/orient-commons-1.0rc5.jar:$HOME/.m2/repository/com/orientechnologies/orientdb-core/1.0rc5/orientdb-core-1.0rc5.jar"
+
+rm -rf target/generated-frascati-*
+
+frascati compile "src/main" "target/$ROLE" $LIB
+frascati run "../composites/$ROLE$1.composite" -libpath "target/$ROLE.jar" $LIB
